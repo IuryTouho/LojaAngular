39aede9a1129511a42e676ec4338f87d66bbbb7d
reciclagem <reciclagem@keysystems.com.br>
2024-06-04 17:29:33 -0300

    squash! Pass data to a child component

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  //standalone: true,
+  //imports: [],
+  templateUrl: './product-alerts.component.html',
+  styleUrl: './product-alerts.component.css'
+})
+export class ProductAlertsComponent {
+
+  @Input() product: Product | undefined;
+
+}
