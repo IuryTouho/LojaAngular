7fb387ebd47a4c22e5b90f5f00c1a6c5b2c581f8
reciclagem <reciclagem@keysystems.com.br>
2024-06-12 17:51:29 -0300

    squash! Pass data to a parent component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -11,5 +11,6 @@ import { Product } from '../products';
 export class ProductAlertsComponent {
 
   @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 
 }
